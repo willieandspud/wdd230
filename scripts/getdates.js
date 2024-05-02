ddec781986961b99529bca0c99e941fe94458b78
willieandspud <llo18008@byui.edu>
2024-05-02 17:16:24 -0600

    javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,2 @@
-// getdates.js
-
-// Get the current year
-var currentYear = new Date().getFullYear();
-
-// Update the copyright year in the footer
-document.getElementById("copyright").innerHTML = "&copy;" + currentYear + " Ryan Lloyd, Idaho, USA <img src='us-flag.png' alt='US Flag'>";
-
-// Get the last modified date
-var lastModifiedDate = document.lastModified;
-
-// Update the last modified date in the footer
-document.getElementById("lastModified").textContent = "Last modified: " + lastModifiedDate;
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
