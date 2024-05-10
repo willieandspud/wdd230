da63f3e3020834ccfecb1cc50167c33315163b8a
willieandspud <llo18008@byui.edu>
2024-05-09 18:45:21 -0600

    w03

---
+++ scripts/hamburger_button.js
@@ -0,0 +1,7 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('_______', () => {
+	navigation._______.toggle('_______');
+	hamButton.classList.toggle('open');
+});
