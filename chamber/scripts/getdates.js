d2d4c9c72e94743153c8a9dae6454a210bfafb90
willieandspud <llo18008@byui.edu>
2024-05-18 21:13:52 -0600

    scripts

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,6 @@
+// lastModified
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last Modification: " + text;
+
+// Copyright
+document.getElementById("copyrightYear").innerHTML = new Date().getFullYear();
