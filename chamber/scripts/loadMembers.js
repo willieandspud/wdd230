580a25c53f7dd9ade52e323b243bb310cd638bfe
willieandspud <llo18008@byui.edu>
2024-07-08 09:03:54 -0600

    new

--- chamber/scripts/loadMembers.js
+++ chamber/scripts/loadMembers.js
@@ -6,15 +6,41 @@ document.addEventListener('DOMContentLoaded', function () {
             data.forEach(member => {
                 const memberCard = document.createElement('div');
                 memberCard.classList.add('member-card');
-                memberCard.innerHTML = `
-                    <img src="images/${member.image}" alt="${member.name}">
-                    <h2>${member.name}</h2>
-                    <p>${member.address}</p>
-                    <p>${member.phone}</p>
-                    <p><a href="${member.website}" target="_blank">${member.website}</a></p>
-                    <p>Membership Level: ${member.membership_level}</p>
-                    <p>${member.description}</p>
-                `;
+
+                
+                const infoDiv1 = document.createElement('div');
+                const name = document.createElement('h2');
+                const address = document.createElement('p');
+                const phone = document.createElement('p');
+                const website = document.createElement('a');
+
+                name.textContent = member.name;
+                address.textContent = member.address;
+                phone.textContent = member.phone;
+                website.href = member.website;
+                website.target = "_blank";
+                website.textContent = member.website;
+
+                infoDiv1.appendChild(name);
+                infoDiv1.appendChild(address);
+                infoDiv1.appendChild(phone);
+                infoDiv1.appendChild(website);
+
+                const infoDiv2 = document.createElement('div');
+                const membershipLevel = document.createElement('p');
+                const description = document.createElement('p');
+
+                membershipLevel.textContent = `Membership Level: ${member.membership_level}`;
+                description.textContent = member.description;
+
+                infoDiv2.appendChild(membershipLevel);
+                infoDiv2.appendChild(description);
+
+                
+                memberCard.appendChild(infoDiv1);
+                memberCard.appendChild(infoDiv2);
+
+                
                 memberDirectory.appendChild(memberCard);
             });
         })
